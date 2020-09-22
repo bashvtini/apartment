bd2c60c9cb6e4e3c662b71bcb8d7d07ef8a3f1f1
bashvtini <bashvtini@gmail.com>
2020-09-22 18:39:33 +0200

    Updates

---
+++ precache-manifest.f447407e04fb9b84e28127441d654016.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7da368b0a804a1d563e42c56508d49e",
+    "url": "/apartment/index.html"
+  },
+  {
+    "revision": "8a68e549a86dd76a01f3",
+    "url": "/apartment/static/css/main.6bfb4474.chunk.css"
+  },
+  {
+    "revision": "7fe5a07de3dc1a8b9336",
+    "url": "/apartment/static/js/2.3ad7d8df.chunk.js"
+  },
+  {
+    "revision": "33ade7f87243ebea1c83e915f94afce3",
+    "url": "/apartment/static/js/2.3ad7d8df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a68e549a86dd76a01f3",
+    "url": "/apartment/static/js/main.b456ad42.chunk.js"
+  },
+  {
+    "revision": "e26edd594c84c60205ee",
+    "url": "/apartment/static/js/runtime-main.5f555557.js"
+  },
+  {
+    "revision": "915d2cd3eaab6acd112241a4f7500e85",
+    "url": "/apartment/static/media/1.915d2cd3.jpg"
+  },
+  {
+    "revision": "515994c30d08af4d76ef414161d69f9d",
+    "url": "/apartment/static/media/2.515994c3.jpg"
+  },
+  {
+    "revision": "40e349f245e78a2fbb40bc94eb17f2fb",
+    "url": "/apartment/static/media/3.40e349f2.jpg"
+  },
+  {
+    "revision": "f149dde505b133abdcae38f00eb4331f",
+    "url": "/apartment/static/media/4.f149dde5.jpg"
+  },
+  {
+    "revision": "5ff1f2a9a78730d7d0c309320ff3c9c7",
+    "url": "/apartment/static/media/Inter-Medium-slnt=0.5ff1f2a9.ttf"
+  },
+  {
+    "revision": "515cae74eee4925d56e6ac70c25fc0f6",
+    "url": "/apartment/static/media/Inter-Regular-slnt=0.515cae74.ttf"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/apartment/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "c9b153f4cf9c55c99dce18211c50d3a1",
+    "url": "/apartment/static/media/bath-solid.c9b153f4.svg"
+  },
+  {
+    "revision": "d21cabf74298bcedcc2324640f6b8f7c",
+    "url": "/apartment/static/media/bed-solid.d21cabf7.svg"
+  },
+  {
+    "revision": "066a9e96c3b6b4715385d336d7dce96e",
+    "url": "/apartment/static/media/crop-alt-solid.066a9e96.svg"
+  },
+  {
+    "revision": "3838ffdd0af44c3f493d45b60008f58d",
+    "url": "/apartment/static/media/logo.3838ffdd.svg"
+  },
+  {
+    "revision": "c6a0effbe867db0941bd5592f8f43810",
+    "url": "/apartment/static/media/search.c6a0effb.svg"
+  },
+  {
+    "revision": "007672494095d9805386f67150d911b3",
+    "url": "/apartment/static/media/sort-amount-down-alt-solid.00767249.svg"
+  }
+]);
